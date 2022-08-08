78d2aa344f4b195dfd742125ff4f441126e19480
Lukasz Konatowski <konat1993@gmail.com>
2022-08-08 20:39:34 +0200

    working with config param (3)

--- src/api/post/policies/check-role.js
+++ src/api/post/policies/check-role.js
@@ -1,16 +1,16 @@
 'use strict'
 
 /**
- * `is-admin` policy.
+ * `check-role` policy.
  */
 
 module.exports = (policyContext, config, { strapi }) => {
-  // policyContext -> have info about our user
-
-  const isEligible = policyContext.state.user && policyContext.state.user.role.code === 'Administrator' // 'Administrator'
+  console.log('config: ', config)
+  const { userRole } = config
+  const isEligible = policyContext.state.user && policyContext.state.user.role.code === userRole // 'Administrator'
 
   // Add your own logic here.
-  strapi.log.info('In is-admin policy mate.')
+  // strapi.log.info('In is-admin policy mate.')
   console.log('policy', policyContext.state)
   console.log('iseliglable', isEligible)
   if (isEligible) {
--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -9,7 +9,7 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 module.exports = createCoreRouter('api::post.post', {
     config: {
         find: {
-            policies: ['is-admin'] // now our new policy is executed by this route
+            policies: [{name: 'check-role', config: {userRole: "Author"}}] // now our new policy is executed by this route
             // policies: ['api::tag.is-admin'] --> if we have another api e.q. tag er have to declare it like this
         }
     }
