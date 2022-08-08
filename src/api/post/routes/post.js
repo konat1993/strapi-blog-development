8886053317a640d656ee21f91a1f2a47afd63d93
Lukasz Konatowski <konat1993@gmail.com>
2022-08-08 20:15:41 +0200

    policy setup by yarn strapi generate command (1 step)

--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -6,4 +6,11 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::post.post');
+module.exports = createCoreRouter('api::post.post', {
+    config: {
+        find: {
+            policies: ['is-admin'] // now our new policy is executed by this route
+            // policies: ['api::tag.is-admin'] --> if we have another api e.q. tag er have to declare it like this
+        }
+    }
+});
