2cb84bef069260bb5e4e36d0a7951cd5e0aba906
Lukasz Konatowski <konat1993@gmail.com>
2022-08-09 15:18:46 +0200

    clear prev commits

--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -6,11 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::post.post', {
-    config: {
-        find: {
-            policies: [{name: 'check-role', config: {userRole: "Author"}}] // now our new policy is executed by this route
-            // policies: ['api::tag.is-admin'] --> if we have another api e.q. tag er have to declare it like this
-        }
-    }
-});
+module.exports = createCoreRouter('api::post.post');
