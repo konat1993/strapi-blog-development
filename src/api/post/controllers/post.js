5d2cdfee07b4825fa82d987a0df7f3344104f50f
Lukasz Konatowski <konat1993@gmail.com>
2022-08-12 21:41:35 +0200

    working with controllers: 1 - creating custom action; 2 - wrapping a core action (modify) => recommended 3 - Replacing a core action

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
 /**
  *  post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
 module.exports = createCoreController('api::post.post', ({ strapi }) => ({
     // Method 1: Creating an entirely custom action
@@ -22,24 +22,37 @@ module.exports = createCoreController('api::post.post', ({ strapi }) => ({
     async find(ctx) {
         // some custom logic here
         ctx.query = { ...ctx.query, local: 'en' }
+        console.log('ctx: ', ctx.query)
+        //example:
+        // /api/posts?publicationState=preview:
+        /*
+        {publicationState: 'preview'}
+        {publicationState: 'preview', local: 'en}
+        It is transforming an incoming request: /api/posts?publicationState=preview&local=en
+        */
 
         // Calling the default core action
-        const { data, meta } = await super.find(ctx)
+        const { data, meta } = await super.find(ctx) //https://docs.strapi.io/developer-docs/latest/development/backend-customization/controllers.html#extending-core-controllers
+        // 'super' is calling core find action of the existing controller object also including our above modification
+
 
         // some more custom logic
         meta.date = Date.now()
 
         return { data, meta }
+        // return {
+        //     data: data.map(dataEl => ({ ...dataEl.attributes, id: dataEl.id })),
+        //     meta
+        // }
     },
 
     // Method 3: Replacing a core action
-    async findOne(ctx) {
+    async findOne(ctx) { // /api/posts/:id
         const { id } = ctx.params
         const { query } = ctx
-
-        const entity = await strapi.service('api::post.post').findOne(id, query)
+        const entity = await strapi.service('api::post.post').findOne(id, query) //over-writing core findOne action
         const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
 
         return this.transformResponse(sanitizedEntity)
     }
-}));
+}))
