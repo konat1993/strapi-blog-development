892c19eddb0bcb653071d2945454f2adc53dee18
Lukasz Konatowski <konat1993@gmail.com>
2022-08-13 22:30:19 +0200

    (2nd solution - better then 1st): implementing handler for premium posts for authenticated users

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -7,15 +7,27 @@
 const { createCoreController } = require('@strapi/strapi').factories
 
 module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+
+    // 1st solution:
+    // async find(ctx) {
+    //     const { data, meta } = await super.find(ctx)
+    //     if (ctx.state.user) {
+    //         return { data, meta }
+    //     } else {
+    //         const filterNoPremiumPosts = data.filter(dataEl => !dataEl.attributes.premium)
+    //         return { data: filterNoPremiumPosts, meta }
+    //     }
+    // }
+    // 2nd solution (better than 1st one):
     async find(ctx) {
-        const { data, meta } = await super.find(ctx)
+        if (ctx.state.user) return await super.find(ctx)
 
-        if (ctx.state.user) {
-            return { data, meta }
-        } else {
-            const filterNoPremiumPosts = data.filter(dataEl => !dataEl.attributes.premium)
-            return { data: filterNoPremiumPosts, meta }
-        }
-
+        const filterNoPremiumPosts = await strapi.service('api::post.post').find({
+            filters: {
+                premium: false
+            },
+        })
+        const sanitizedPosts = await this.sanitizeOutput(filterNoPremiumPosts, ctx)
+        return this.transformResponse(sanitizedPosts)
     }
 }))
