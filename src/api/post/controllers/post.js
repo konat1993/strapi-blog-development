1443a232dabe78e26c23ad994abd879a94f4e832
Lukasz Konatowski <konat1993@gmail.com>
2022-08-13 21:53:49 +0200

    implementing handler for premium posts for authenticated users (1st idea - NOT recommended)

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+    async find(ctx) {
+        const { data, meta } = await super.find(ctx)
+
+        if (ctx.state.user) {
+            return { data, meta }
+        } else {
+            const filterNoPremiumPosts = data.filter(dataEl => !dataEl.attributes.premium)
+            return { data: filterNoPremiumPosts, meta }
+        }
+
+    }
+}))
