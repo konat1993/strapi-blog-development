95428968b37f018e89860c38bd502469066ee5b6
Lukasz Konatowski <konat1993@gmail.com>
2022-08-13 23:55:26 +0200

    findOne method for no premium post - no filter acceptance (2ndt solution)

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -10,9 +10,9 @@ module.exports = createCoreController('api::post.post', ({ strapi }) => ({
 
     // 1st solution:
     // async find(ctx) {
-    //     const { data, meta } = await super.find(ctx)
     //     if (ctx.state.user) {
     //         return { data, meta }
+    //     const { data, meta } = await super.find(ctx)
     //     } else {
     //         const filterNoPremiumPosts = data.filter(dataEl => !dataEl.attributes.premium)
     //         return { data: filterNoPremiumPosts, meta }
@@ -54,13 +54,24 @@ module.exports = createCoreController('api::post.post', ({ strapi }) => ({
         return this.transformResponse(sanitizedPosts)
     },
 
+    // async findOne(ctx) {
+    //     const { id } = ctx.params
+    //     if (ctx.state.user) return await super.findOne(ctx, id)
+
+    //     const { data, meta } = await super.findOne(ctx, id)
+    //     const publicPost = data.attributes.premium ? null : data
+    //     return { data: publicPost, meta }
+    // }
+
     async findOne(ctx) {
         const { id } = ctx.params
-        if (ctx.state.user) return await super.findOne(ctx, id)
+        const { query } = ctx
 
-        const { data, meta } = await super.findOne(ctx, id)
-        const publicPost = data.attributes.premium ? null : data
-        return { data: publicPost, meta }
+        if (ctx.state.user) return await super.findOne(ctx)
+
+        const postIfPublic = await strapi.service('api::post.post').findOneIfPublic({ id, query })
+        const sanitizedPost = await this.sanitizeOutput(postIfPublic, ctx)
+        return this.transformResponse(sanitizedPost)
     }
 }))
 
--- src/api/post/services/post.js
+++ src/api/post/services/post.js
@@ -18,5 +18,12 @@ module.exports = createCoreService('api::post.post', ({ strapi }) => ({
         const publicPosts = await strapi.entityService.findMany('api::post.post', this.getFetchParams(newQuery))
 
         return publicPosts
+    },
+
+    async findOneIfPublic(args) {
+        const { id, query } = args
+        const post = await strapi.entityService.findOne('api::post.post', id, this.getFetchParams(query))
+        const publicPost = post.premium ? null : post
+        return publicPost
     }
 }))
