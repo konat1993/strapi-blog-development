c450df7ac5efcb5191107f4aa3f306ca62fed7f5
Lukasz Konatowski <konat1993@gmail.com>
2022-08-20 16:15:04 +0200

    using Strapi design system (strapi table)

--- src/plugins/github-projects/admin/src/components/Repo/Repo.js
+++ src/plugins/github-projects/admin/src/components/Repo/Repo.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import { Table, Thead, Tbody, Tr, Td, Th } from '@strapi/design-system/Table'
+import { Box, BaseCheckbox, Typography } from '@strapi/design-system'
+
+const COL_COUNT = 5
+const ROW_COUNT = 6
 
 const Repo = () => {
     return (
-        <div>Repo component</div>
+        <Box padding={8} background="neutral100">
+            <Table colCount={COL_COUNT} rowCount={ROW_COUNT}>
+                <Thead>
+                    <Tr>
+                        <Th>
+                            <BaseCheckbox aria-label="Select all entries" />
+                        </Th>
+                        <Th>
+                            <Typography variant="sigma">Name</Typography>
+                        </Th>
+                        <Th>
+                            <Typography variant="sigma">Description</Typography>
+                        </Th>
+                        <Th>
+                            <Typography variant="sigma">Url</Typography>
+                        </Th>
+                        <Th>
+                            <Typography variant="sigma">Actions</Typography>
+                        </Th>
+                        {/* <Th>
+                  <VisuallyHidden>Actions</VisuallyHidden>
+                </Th> */}
+                    </Tr>
+                </Thead>
+                {/* <Tbody>
+              {entries.map(entry => <Tr key={entry.id}>
+                  <Td>
+                    <BaseCheckbox aria-label={`Select ${entry.contact}`} />
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">{entry.id}</Typography>
+                  </Td>
+                  <Td>
+                    <Avatar src={entry.cover} alt={entry.contact} />
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">{entry.description}</Typography>
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">{entry.category}</Typography>
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">{entry.contact}</Typography>
+                  </Td>
+                  <Td>
+                    <Flex>
+                      <IconButton onClick={() => console.log('edit')} label="Edit" noBorder icon={<Pencil />} />
+                      <Box paddingLeft={1}>
+                        <IconButton onClick={() => console.log('delete')} label="Delete" noBorder icon={<Trash />} />
+                      </Box>
+                    </Flex>
+                  </Td>
+                </Tr>)}
+            </Tbody> */}
+            </Table>
+        </Box>
     )
 }
 
