e16b0a97594f7614d419536d04dc7002082f46fe
Lukasz Konatowski <konat1993@gmail.com>
2022-08-20 15:44:19 +0200

    Admin panel: plugin icon and first component

---
+++ src/plugins/github-projects/admin/src/components/Repo/Repo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Repo = () => {
+    return (
+        <div>Repo component</div>
+    )
+}
+
+export default Repo
--- src/plugins/github-projects/admin/src/pages/HomePage/index.js
+++ src/plugins/github-projects/admin/src/pages/HomePage/index.js
@@ -5,14 +5,12 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import Repo from '../../components/Repo/Repo'
 
 const HomePage = () => {
   return (
     <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+      <Repo />
     </div>
   );
 };
