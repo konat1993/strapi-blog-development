db0ef9baa031262366709206c6c203c4c2a5323e
Lukasz Konatowski <konat1993@gmail.com>
2022-08-23 10:35:02 +0200

    Applied proper auth policy to route

--- src/plugins/github-projects/server/routes/index.js
+++ src/plugins/github-projects/server/routes/index.js
@@ -4,8 +4,8 @@ module.exports = [
     path: '/repos',  //=> available at route: //localhost:1337/github-projects/repos
     handler: 'getReposController.index',
     config: {
-      policies: [],
-      auth: false, // this is temporary disabled for testing route. Later on we will handle authorized service for only admin panel users
+      policies: ['admin::isAuthenticatedAdmin'],
+      // auth: false, // this is temporary disabled for testing route. Later on we will handle authorized service for only admin panel users
     },
   },
 ];
